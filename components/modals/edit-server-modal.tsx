3a92a5fca0e4077ba12e59ead9ca0922aceee0b8
DevHex007 <olahex@gmail.com>
2023-09-29 07:59:20 +0100

    Update edit-server-modal.tsx

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -134,7 +134,7 @@ export const EditServerModal = () => {
               />
             </div>
             <DialogFooter className="bg-gray-100 px-6 py-4">
-              <Button variant="default" disabled={isLoading}>
+              <Button variant="primary" disabled={isLoading}>
                 Save
               </Button>
             </DialogFooter>
